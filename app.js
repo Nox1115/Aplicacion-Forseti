b3604b90468802b6ce919f5843421cbcd70bd726
stock2342 <jromeroor@unal.edu.co>
2022-02-01 19:26:45 -0500

    Actualización funcionamiento app.js Versión final del prototipo 1

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
-var setAnswers = function(idFormularioPregunta){
-    var respuesta = null;
-    let opciones = document.querySelectorAll('input[type="radio"]');
-
-    document.querySelector(idFormularioPregunta).addEventListener('change',()=>{
-
-    opciones.forEach(opcion =>{
-
-        if(opcion.checked){
-            respuesta = opcion.value;
-            return respuesta;
-        }
-    })
-})
-};
-
-
+var nombreEncuestado = document.getElementById('nombre');
 document.getElementById('BotonEnviar').addEventListener('click', ()=>{
-    var nombreEncuestado = document.getElementById('nombre').value();
-    var respuestaPregunta1 = setAnswers('pregunta1');
-    var respuestaPregunta2 = setAnswers('pregunta2');
-    var respuestaPregunta3 = setAnswers('pregunta3');
-    var respuestaPregunta4 = setAnswers('pregunta4');
-    var respuestaPregunta5 = setAnswers('pregunta5');
-
-    var respuestasGuardadas = nombreEncuestado + ' sus respuestas:[' + respuestaPregunta1 + ', ' + respuestaPregunta2 + ', ' + respuestaPregunta3 + ', ' + respuestaPregunta4 + ', ' + respuestaPregunta5 + '].';
+    var nombre = nombreEncuestado.value
+    var respuestaPregunta1 = setAnswers("preg1");
+    var respuestaPregunta2 = setAnswers("preg2");
+    var respuestaPregunta3 = setAnswers("preg3");
+    var respuestaPregunta4 = setAnswers("preg4");
+    var respuestaPregunta5 = setAnswers("preg5");
+    //var respuestasGuardadas = nombre + ' sus respuestas: [' + respuestaPregunta1 + '].';
+    var respuestasGuardadas = nombre + ' sus respuestas:[' + respuestaPregunta1 + ', ' + respuestaPregunta2 + ', ' + respuestaPregunta3 + ', ' + respuestaPregunta4 + ', ' + respuestaPregunta5 + '].';
     var blob = new Blob([respuestasGuardadas], {
       type:"application/json;utf - 8"
     })
@@ -32,3 +16,13 @@ document.getElementById('BotonEnviar').addEventListener('click', ()=>{
     userLink.setAttribute('href', window.URL.createObjectURL(blob));
     userLink.click();
 })
+
+var setAnswers = function(numberPreg){
+      var opciones = document.getElementsByName(numberPreg);
+      for(i=0; i<opciones.length; i++){
+        if(opciones[i].checked){
+          return opciones[i].value;
+        }
+      }
+
+};
